fa1ad8eb17ead5bd86375dfbd23bd59b406f2564
Jaehyeon <fourhf1@gmail.com>
2020-02-17 03:48:09 +0900

    first commit

---
+++ app/src/main/java/com/myapplication/ui/category/CategoryFragment.kt
@@ -0,0 +1,31 @@
+package com.myapplication.ui.category
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.myapplication.R
+
+class CategoryFragment : Fragment() {
+
+    private lateinit var categoryViewModel: CategoryViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        categoryViewModel =
+            ViewModelProviders.of(this).get(CategoryViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_category, container, false)
+        val textView: TextView = root.findViewById(R.id.text_category)
+        categoryViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
